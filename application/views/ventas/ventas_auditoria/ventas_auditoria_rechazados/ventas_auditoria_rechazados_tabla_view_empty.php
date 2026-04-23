8d110606568362bde09d12f9ba7e19ebcde11814
LuisMandujanoSX <luis.mandujano@strategix.com.mx>
2026-04-23 09:38:19 -0600

    ajuste en módulo de tickets rechazados

--- application/views/ventas/ventas_auditoria/ventas_auditoria_rechazados/ventas_auditoria_rechazados_tabla_view_empty.php
+++ application/views/ventas/ventas_auditoria/ventas_auditoria_rechazados/ventas_auditoria_rechazados_tabla_view_empty.php
@@ -7,15 +7,13 @@
  * @CreateDate 01 MARZO 2026 09:00:00                        * 
  */
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 ?>
 <section class="auditoria_ventas">
-    <div class="container">
-        <div class="panel-white">
-            <div class="row">
-                SEM INFORMAÇÃO
-            </div>
+    <div class="panel-white">
+        <div class="row">
+            SEM INFORMAÇÃO
         </div>
     </div>
 </section>

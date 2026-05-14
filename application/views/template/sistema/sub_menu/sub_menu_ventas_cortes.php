d236a9e603328063c616153019b3d46186ea4ae2
LuisMandujanoSX <luis.mandujano@strategix.com.mx>
2026-05-14 13:19:15 -0600

    diseño corte de auditoría de ventas

--- application/views/template/sistema/sub_menu/sub_menu_ventas_cortes.php
+++ application/views/template/sistema/sub_menu/sub_menu_ventas_cortes.php
@@ -12,30 +12,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 
-<div class="col-lg-3">
+<div class="col-lg-3 menu-cortes mt-3 mb-3">
     <a href="<?php echo funciones_strategix_version_url_random_base_url("CorteAuditoriaVentas") ?>">
-        <button type="button" class="btn btn-gray"><?= $this->lang->line('menu_submenu_admin_corte_auditoria_ventas') ?>
+        <button type="button" class="btn btn-axalta"><?= $this->lang->line('menu_submenu_admin_corte_auditoria_ventas') ?>
         </button>
     </a>
 </div>
-<div class="col-lg-3">
+<div class="col-lg-3 menu-cortes mt-3 mb-3">
     <a href="<?php echo funciones_strategix_version_url_random_base_url("CorteGanadoresVentas") ?>">
         <button type="button"
-            class="btn btn-gray"><?= $this->lang->line('menu_submenu_admin_reposicion_prodcutos_generacion_ganadores') ?>
+            class="btn btn-axalta"><?= $this->lang->line('menu_submenu_admin_reposicion_prodcutos_generacion_ganadores') ?>
         </button>
     </a>
 </div>
-<div class="col-lg-3">
+<div class="col-lg-3 menu-cortes mt-3 mb-3">
     <a href="<?php echo funciones_strategix_version_url_random_base_url("CorteVentasBimestral") ?>">
-        <button type="button" class="btn btn-gray">
+        <button type="button" class="btn btn-axalta">
             <?= $this->lang->line('menu_submenu_admin_cortes_bimestral') ?>
         </button>
     </a>
 </div>
-<div class="col-lg-3">
+<div class="col-lg-3 menu-cortes mt-3 mb-3">
     <a href="<?php echo funciones_strategix_version_url_random_base_url("AperturaCierreRepProd") ?>">
-        <button type="button" class="btn btn-gray">
-             <?= $this->lang->line('menu_submenu_admin_apertura_cierre_reposicion_producto') ?>
+        <button type="button" class="btn btn-axalta">
+            <?= $this->lang->line('menu_submenu_admin_apertura_cierre_reposicion_producto') ?>
         </button>
     </a>
 </div>

06df3effe38afe6cce6d52087d80a361d6a63a35
Luis Felipe Rangel <luis.rangel@strategix.com.mx>
2026-09-04 09:58:50 -0600

    2026

---
+++ application/language/portuguese/reportes_auditoria_promociones_controller_lang.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['reportes_auditoria_promociones_titulo'] = 'RELATÓRIO DE AUDITORIA DE PROMOÇÕES';
+$lang['reportes_auditoria_promociones_filtro_anio'] = 'Ano Registro:';
+$lang['reportes_auditoria_promociones_filtro_mes'] = 'Mês Registro:';
+$lang['reportes_auditoria_promociones_filtro_distribuidor'] = 'Distribuidor:';
+$lang['reportes_auditoria_promociones_filtro_estatus'] = 'Estatus:';
+$lang['reportes_auditoria_promociones_btn_buscar'] = 'PROCURAR';
+$lang['reportes_auditoria_promociones_estatus_todos'] = 'TODOS';
+$lang['reportes_auditoria_promociones_estatus_en_auditoria'] = 'PENDENTE';
+$lang['reportes_auditoria_promociones_estatus_aprobada'] = 'APROVADA';
+$lang['reportes_auditoria_promociones_estatus_rechazada'] = 'REJEITADA';
+$lang['reportes_auditoria_promociones_th_ventano'] = 'NO.';
+$lang['reportes_auditoria_promociones_th_ventaid'] = 'ID';
+$lang['reportes_auditoria_promociones_th_pintor'] = 'PINTOR';
+$lang['reportes_auditoria_promociones_th_numero_ticket'] = 'Nº DE TICKET';
+$lang['reportes_auditoria_promociones_th_monto_ticket'] = 'VALOR DO TICKET';
+$lang['reportes_auditoria_promociones_th_fecha_registro'] = 'DATA DE REGISTRO';
+$lang['reportes_auditoria_promociones_th_distribuidor'] = 'DISTRIBUIDOR';
+$lang['reportes_auditoria_promociones_th_ticket'] = 'TICKET';
+$lang['reportes_auditoria_promociones_th_motivo'] = 'MOTIVO';
+$lang['reportes_auditoria_promociones_th_ticket_monto_repetido'] = 'TICKETS VALOR REPETIDO';
+$lang['reportes_auditoria_promociones_th_estatus_auditoria'] = 'STATUS DA AUDITORIA';
+$lang['reportes_auditoria_promociones_th_observaciones'] = 'OBSERVAÇÕES';
+$lang['reportes_auditoria_promociones_th_aprobar'] = 'APROVAR';
+$lang['reportes_auditoria_promociones_th_rechazar'] = 'REJEITAR';
+$lang['reportes_auditoria_promociones_th_nombre'] = 'NOME';
+$lang['reportes_auditoria_promociones_th_ticket_modal'] = 'BILHETE';
+$lang['reportes_auditoria_promociones_th_total_montos'] = 'TOTAL VALORES';
+$lang['reportes_auditoria_promociones_th_total_cantidades'] = 'TOTAL QUANTIDADES';
+$lang['reportes_auditoria_promociones_th_total_litros'] = 'TOTAL LITROS';
+$lang['reportes_auditoria_promociones_th_fecha_auditoria'] = 'DATA DE AUDITORIA';
